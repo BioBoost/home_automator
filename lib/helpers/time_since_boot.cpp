aee596b32204407db732d3eba0398793e2c0e758
Nico De Witte <nico.dewitte@outlook.com>
2018-07-03 19:34:45 +0200

    Convert boot timer into singleton that is globally accessible

--- lib/helpers/time_since_boot.cpp
+++ lib/helpers/time_since_boot.cpp
@@ -1,12 +1,24 @@
 #include "time_since_boot.h"
 
 namespace Helpers {
+  
+  void TimeSinceBoot::init(void) {
+    // Does nothing but call to get_instance() will initialize
+    // the instance
+  }
+
+  TimeSinceBoot& TimeSinceBoot::get_instance() {
+      static TimeSinceBoot instance;   // Guaranteed to be destroyed.
+      return instance;
+  }
 
   TimeSinceBoot::TimeSinceBoot(void) {
     millisSinceBoot = 0;
     resetTicker.attach(callback(this, &TimeSinceBoot::overflow_protection), 600);
     timer.start();
   }
+  
+  TimeSinceBoot::~TimeSinceBoot(void) { }
 
   uint64_t TimeSinceBoot::millis_since_boot(void) {
     return (millisSinceBoot + timer.read_ms());
@@ -18,3 +30,4 @@ namespace Helpers {
   }
 
 };
+
--- lib/helpers/time_since_boot.h
+++ lib/helpers/time_since_boot.h
@@ -2,18 +2,39 @@
 
 #include "mbed.h"
 
+#define BootTimer Helpers::TimeSinceBoot::get_instance()
+
 namespace Helpers {
 
-  class TimeSinceBoot {
+  class TimeSinceBoot final {
 
     public:
-      TimeSinceBoot(void);
+      void init(void);    // Must be called on system boot to initialize instance
+
+    public:
+      static TimeSinceBoot& get_instance();
+
+    private:
+        TimeSinceBoot(void);   // Make constructor private
+        ~TimeSinceBoot(void);
 
     public:
       uint64_t millis_since_boot(void);
 
     private:
       void overflow_protection(void);
+
+    // Dont forget to declare these two. You want to make sure they
+    // are unacceptable otherwise you may accidentally get copies of
+    // your singleton appearing.
+    public:
+        TimeSinceBoot(TimeSinceBoot const&)   = delete;
+        void operator=(TimeSinceBoot const&)  = delete;
+        // Note: Scott Meyers mentions in his Effective Modern
+        //       C++ book, that deleted functions should generally
+        //       be public as it results in better error messages
+        //       due to the compilers behavior to check accessibility
+        //       before deleted status
 
     private:
       Timer timer;
